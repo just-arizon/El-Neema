3df6d0b23302a5064f11fa1e651fde2c206cf67b
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2024-09-02 01:24:43 +0100

    flutter-gateway test commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,11 @@ import About from './components/AboutUs';
 import Footer from './components/Footer';
 import Donation from './components/Donation';
 import { FlutterWaveButton, closePaymentModal } from 'flutterwave-react-v3';
+import Logo from "../public/favicon-32x32.png";
 
 function App() {
   const config = {
-    public_key: 'FLWPUBK-**************************-X',
+    public_key: import.meta.env.VITE_FLUTTERWAVE_PUBLIC_KEY,
     tx_ref: Date.now(),
     amount: 100,
     currency: 'NGN',
@@ -21,9 +22,9 @@ function App() {
       name: 'john doe',
     },
     customizations: {
-      title: 'My store',
+      title: 'El-Neema Donation',
       description: 'Payment for items in cart',
-      logo: 'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg',
+      logo: Logo,
     },
   };
 
